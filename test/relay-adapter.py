7746fe83d124526e49140eb8255eb75efd444625
Trent Larson <trent@trentlarson.com>
2013-07-23 19:55:05 -0400

    add adapter for old (bad) relay; other refactorings

--- test/relay-adapter.py
+++ test/relay-adapter.py
@@ -1,6 +1,5 @@
 
-# BEWARE: a blank request (eg. only a newline) signals an end to that whole cilent-server conversation
-
+# Set these values for your relay and servers.
 
 RELAY_SERVER = ('localhost', 8082)
 
@@ -13,13 +12,19 @@ SERVERS = [
 VERBOSE = 1
 
 
+
+# BEWARE: a blank request (eg. only a newline) signals an end to that whole cilent-server conversation
+
 import socket
 import threading
+
+SHUTDOWN_COMMAND = "shutdown adapter"
 
 # Read from the sockets, looking for "\n" (one character at a time).
 def readLine(s):
     return s.makefile().readline().rstrip("\n")
 '''
+    # another approach
     ret = ''
     while True:
         c = s.recv(1)
@@ -55,7 +60,7 @@ class CopyFromSockToSock(threading.Thread):
         try:
             request = readLine(self.clientSock)
             while (request != ''):
-               if (request == 'shutdown relay'):
+               if (request == SHUTDOWN_COMMAND):
                    shutdownAllSockets()
                else:
                     self.serverSock.sendall(request + "\n")
@@ -65,6 +70,11 @@ class CopyFromSockToSock(threading.Thread):
         finally:
             self.clientSock.close()
             self.serverSock.close()
+
+
+
+
+
 
 class RouteThroughRelay(threading.Thread):
     def __init__(self, publicHostAndPort, relaySock, serverHostAndPort):
@@ -77,41 +87,64 @@ class RouteThroughRelay(threading.Thread):
 
         # This is the part of the program where you'll want to customize behavior for this server.
 
-        try:
+        newClientHostPortStr = readLine(self.relaySock)
+        while (newClientHostPortStr != ''):
+            if (VERBOSE):
+                print "New client now available at {} going to {}".format(newClientHostPortStr,
+                                                                          self.serverHostAndPort)
+            newClientHostAndPort = hostAndPortTuple(newClientHostPortStr)
+            
+            clientRelaySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            clientRelaySock.connect(newClientHostAndPort)
+            socketsToShutdown.append(clientRelaySock)
+            
+            serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serverSock.connect(self.serverHostAndPort)
+            socketsToShutdown.append(serverSock)
+            
+            CopyFromSockToSock(clientRelaySock, serverSock).start()
+
             newClientHostPortStr = readLine(self.relaySock)
-            while (newClientHostPortStr != ''):
-                if (VERBOSE):
-                    print "New client now available at {} going to {}".format(newClientHostPortStr,
-                                                                              self.serverHostAndPort)
-                newClientHostAndPort = hostAndPortTuple(newClientHostPortStr)
+
+
+
+
+class RouteThroughOldRelay(threading.Thread):
+    def __init__(self, publicHostAndPort, relaySock, serverHostAndPort):
+        super(RouteThroughOldRelay, self).__init__()
+        self.publicHostAndPort = publicHostAndPort
+        self.serverHostAndPort = serverHostAndPort
+        self.relaySock = relaySock
+    def run(self):
+
+        # This is the part of the program where you'll want to customize behavior for this server.
+
+        serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSock.connect(self.serverHostAndPort)
+        socketsToShutdown.append(serverSock)
             
-                clientRelaySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                clientRelaySock.connect(newClientHostAndPort)
-                socketsToShutdown.append(clientRelaySock)
-            
-                serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                serverSock.connect(self.serverHostAndPort)
-                socketsToShutdown.append(serverSock)
-            
-                CopyFromSockToSock(clientRelaySock, serverSock).start()
-
-                newClientHostPortStr = readLine(self.relaySock)
-
-        finally:
-            pass
-#            shutdownAllSockets()
+        CopyFromSockToSock(self.relaySock, serverSock).start()
 
 
-print "To shutdown, connect to any public port and enter: shutdown relay"
+
+
+
+
+
+
+print "To shutdown, connect to any public port and enter:", SHUTDOWN_COMMAND
 
 # For each server, get a connection to the relay.
 for idx, serverHostAndPort in enumerate(SERVERS):
     relaySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     relaySock.connect(RELAY_SERVER)
+    socketsToShutdown.append(relaySock)
+
     # get the HOST:PORT for this server's public address
     publicHostAndPort = hostAndPortTuple(readLine(relaySock))
-    # let's show the public address for this server
     if (VERBOSE):
         print "server {}={} is public at {}".format(idx, serverHostAndPort, publicHostAndPort)
+
     RouteThroughRelay(publicHostAndPort, relaySock, serverHostAndPort).start()
+    #RouteThroughOldRelay(publicHostAndPort, relaySock, serverHostAndPort).start()
     
